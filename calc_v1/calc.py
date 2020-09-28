aa350f9c817dee3219d10a8b62c971b06e464f6c
Seberyak <v_liparteliani@cu.edu.ge>
2020-09-28 21:07:46 +0400

    optimized with pylint

--- calc_v1/calc.py
+++ calc_v1/calc.py
@@ -7,27 +7,28 @@ ERROR_MSG = "(Error: {})"
 def check(args, arg):
     if args[0] == "+":
         return arg + float(args[1])
-    elif args[0] == "-":
+    if args[0] == "-":
         return arg - float(args[1])
-    elif args[0] == "*":
+    if args[0] == "*":
         return arg * float(args[1])
-    elif args[0] == "/":
+    if args[0] == "/":
         return arg / float(args[1])
+    return None
 
 
 if __name__ == '__main__':
-    answer = 0
+    ANSWER = 0
     try:
         arguments = re.split(r'(\D)', sys.argv[1])
 
-        for i in range(len(arguments)):
+        for i, el in enumerate(arguments):
             if i == 0:
-                answer = float(arguments[i])
+                ANSWER = float(arguments[i])
 
             elif i % 2 == 0:
-                answer = check(arguments[i - 1:i + 1], answer)
-        answer = int(answer + 0.5)
-        print(answer)
+                ANSWER = check(arguments[i - 1:i + 1], ANSWER)
+        ANSWER = int(ANSWER + 0.5)
+        print(ANSWER)
 
     except Exception as err:
         print(ERROR_MSG.format(err))
